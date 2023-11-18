cb3b351ab66ebaa2934ebcc34a7a31655ab3f741
mvillageman123 <151280832+mvillageman123@users.noreply.github.com>
2023-11-19 00:11:51 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -51,3 +51,15 @@ def get_dataframe(table_name):
     return df
 data=get_dataframe("MYTABLE") 
 streamlit.dataframe(data) 
+
+with st.form("my_form"):
+   st.write("Inside the form")
+   slider_val = st.slider("Form slider")
+   checkbox_val = st.checkbox("Form checkbox")
+
+   # Every form must have a submit button.
+   submitted = st.form_submit_button("Submit")
+   if submitted:
+       st.write("slider", slider_val, "checkbox", checkbox_val)
+
+st.write("Outside the form")
