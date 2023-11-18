cce3150f6059858085ca6144b609ac7ec43d7f7e
mvillageman123 <151280832+mvillageman123@users.noreply.github.com>
2023-11-18 23:40:26 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,46 @@
 import streamlit
 import pandas 
-streamlit.title("🍞 My Parent's Healthy Dinner") 
-streamlit.header('🥗 Breakfast Menu')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🥑 Hard-Boiled Free-Range Egg')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-selected_fruit=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Grapes', 'Lemon']) 
-to_show=my_fruit_list.loc[selected_fruit]
-streamlit.dataframe(to_show)
-
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
 import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+
+#streamlit.title("🍞 My Parent's Healthy Dinner") 
+#streamlit.header('🥗 Breakfast Menu')
+#streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+#streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+#streamlit.text('🥑 Hard-Boiled Free-Range Egg')
+#streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+#my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#my_fruit_list = my_fruit_list.set_index('Fruit')
+#selected_fruit=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Grapes', 'Lemon']) 
+#to_show=my_fruit_list.loc[selected_fruit]
+#streamlit.dataframe(to_show)
+
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+# write your own comment -what does the next line do? 
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+#streamlit.dataframe(fruityvice_normalized)
+
+#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+#streamlit.write('The user entered ', fruit_choice)
+
+#my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+#my_cur = my_cnx.cursor()
+#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+#my_data_row = my_cur.fetchone()
+#streamlit.text("Hello from Snowflake:")
+#streamlit.text(my_data_row)
+
+# streamlit_app.py
+
+import streamlit as st
+
+# Initialize connection.
+conn = st.connection("snowflake")
+
+# Perform query.
+df = conn.query("SELECT * from mytable;", ttl=600)
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.NAME} has a :{row.PET}:")
